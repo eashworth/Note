60cf90214393f247be837ef8da0cc0b9b2e19a8e
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-10-01 16:16:59 +0100

    note list view works for empty list or multiple notes

--- note-list-view-model.js
+++ note-list-view-model.js
@@ -4,13 +4,12 @@
   }
 
   NoteListView.prototype.readNoteList = function() {
-    console.log(this._notelist._list)
     var new_array = this._notelist._list.map(function(element) {
       return "<li><div>" + element._text + "</div></li>"
       });
     var concatenated_string = "<ul>" + new_array.join('') + "</ul>";
     return concatenated_string;
   }
-  
+
   exports.NoteListView = NoteListView;
 })(this);
--- note-list-view-test.js
+++ note-list-view-test.js
@@ -12,3 +12,36 @@
   };
   TestSingleNoteListView();
 })(this);
+
+(function(exports) {
+  function testNoNoteListView() {
+    var notelist = new NoteList;
+    var notelistview = new NoteListView(notelist);
+
+    if (notelistview.readNoteList() !== "<ul></ul>")  {
+      throw new Error("Test has failed")
+    } else {
+      console.log("Well done.")
+    };
+  };
+  testNoNoteListView();
+})(this);
+
+(function(exports) {
+  function testMultipleNotesListView () {
+    var notelist = new NoteList;
+
+    notelist.addNoteToList("Test text")
+    notelist.addNoteToList("Test text 2")
+
+    var notelistview = new NoteListView(notelist);
+
+    if(notelistview.readNoteList() !== "<ul><li><div>Test text</div></li><li><div>Test text 2</div></li></ul>") {
+      throw new Error("Test has failed")
+    } else {
+      console.log("Well done.")
+    };
+  };
+
+  testMultipleNotesListView();
+})(this);
